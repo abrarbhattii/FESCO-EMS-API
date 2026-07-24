886353268eb5a786fc93121fce93f596d1d21f4c
mussadiq12 <mussadiqali911@gmail.com>
2026-07-24 19:29:36 +0500

    a little chnage

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const dbConfig = {
     database: process.env.DB_NAME,
     options: {
         encrypt: true,
-        trustServerCertificate: true
+        trustServerCertificate: false
     }
 };
 
